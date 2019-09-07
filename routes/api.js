1c2d3ca2d25a737d4f96b6a340e8b3f507a7ff43
Shevi <shevids@gmail.com>
2019-09-07 13:23:48 +1000

    Finalised the reservations page

--- routes/api.js
+++ routes/api.js
@@ -6,18 +6,20 @@ var reservation = [
     {
         name: "Bruce",
         email: "Bruce_Willis@gmail.com",
-        number: "0434817232"
+        number: "0434817232",
+        id: "99c12cb1-5251-4f80-9886-c327448575bd"
     },
     {
         name: "Gerard",
         email: "lil_bitch_boi@gmail.com",
-        number: "0434254678"
+        number: "0434254678",
+        id: "9b758ee1-4749-469c-94f9-e20f6216d8af"
     },
 
 ];
 
-router.get("/tables", function(req, res) {
-    var reserved = reservation.slice(0,5);
+router.get("/tables", function (req, res) {
+    var reserved = reservation.slice(0, 5);
     var waiting = reservation.slice(5);
 
     var list = {
@@ -28,8 +30,8 @@ router.get("/tables", function(req, res) {
     res.json(list);
 })
 
-router.post("/reserve", function(req, res) {
-    if(req.body){
+router.post("/reserve", function (req, res) {
+    if (req.body) {
         var id = uuidv1();
         req.body.ID = id;
         reservation.push(req.body);
@@ -37,7 +39,7 @@ router.post("/reserve", function(req, res) {
     } else {
         res.json(false);
     }
-    
+
 });
 
 module.exports = router;
